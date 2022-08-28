5ca091eeed26f3d08a85e327b1255043ee69d8dc
piayer69 <maxmarsz1@gmail.com>
2022-08-28 15:02:38 +0200

    Updated responses, changed post editing

--- api/views.py
+++ api/views.py
@@ -8,6 +8,16 @@ from posts.models import Post
 from .serializers import PostSerializer, UserSerializer
 
 
+def sort_posts(posts):
+    months = posts.dates('date', 'month')
+    sorted_posts = {}
+    for month in months:
+        if sorted_posts.get(month.year) is None:
+            sorted_posts[month.year] = []
+        sorted_posts[month.year].append({month.month: [PostSerializer(post).data for post in posts if post.date.year == month.year and post.date.month == month.month]})
+    return sorted_posts
+
+
 class UserRegisterView(APIView):
     def post(self, request):
         username = request.data['username']
@@ -24,50 +34,36 @@ class UserRegisterView(APIView):
 class ListCreatePostView(APIView):
     def get(self, request):
         posts = Post.objects.all()
-        months = posts.dates('date', 'month')
-        sorted_posts = {}
-
-        for month in months:
-            for post in posts:
-                if post.date.year == month.year and post.date.month == month.month:
-                    if sorted_posts.get(str(month)) is None:
-                        sorted_posts[str(month)] = []
-                    sorted_posts[str(month)].append(PostSerializer(post).data)
-
-        # print(sorted_posts)
-
-        # for month in months:
-        #     if sorted_posts.get(month.year) is None:
-        #         sorted_posts[month.year] = []
-        #     sorted_posts[month.year].append({month.month: [PostSerializer(e).data for e in posts if e.date.year == month.year and e.date.month == month.month]})
         
         # for key, value in sorted_posts.items():
         #     print(key, value)
-        # print(now().year)
         
         # posts_serialized = PostSerializer(posts, many=True)
-        return Response(sorted_posts)
+        return Response(sort_posts(posts))
 
     def post(self, request):
         post = PostSerializer(data=request.data)
         if post.is_valid():
             post.save()
-            return Response(post.data)
+            posts = Post.objects.all()
+            return Response(sort_posts(posts))
         return Response('Something went wrong', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class UpdateDestroyPostView(APIView):
-    def put(self, request, pk):
+    def patch(self, request, pk):
         post = Post.objects.get(id=pk)
-        serialized = PostSerializer(data=request.data, instance=post)
+        serialized = PostSerializer(post, data=request.data, partial=True)
         if serialized.is_valid():
             serialized.save()
-            return Response(serialized.data, status=status.HTTP_200_OK)
+            posts = Post.objects.all()
+            return Response(sort_posts(posts), status=status.HTTP_200_OK)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
         post = Post.objects.get(id=pk)
         post.delete()
+        posts = Post.objects.all()
         if post.id is None:
-            return Response(status=status.HTTP_200_OK)
+            return Response(sort_posts(posts), status=status.HTTP_200_OK)
         return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
